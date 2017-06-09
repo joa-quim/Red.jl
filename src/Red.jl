952b98712a8a4a5f8d8b6dab729fed84f1060634
Joaquim Luis <jmfluis@gmail.com>
2017-06-09 19:51:34 +0100

    Export more funs

--- src/Red.jl
+++ src/Red.jl
@@ -3,9 +3,9 @@ VERSION >= v"0.5" && __precompile__()
 module Red
 
 export
-    redOpen, redClose, redDo, redDoFile, redDoBlock, redRoutine, redSymbol, redUnset,
+    redOpen, redClose, redDo, redDoFile, redDoBlock, redCall, redRoutine, redSymbol, redUnset,
     redNone, redLogic, redDatatype, redInteger, redFloat, redPair, redTouple, redTouple4,
-	redBinary, redString, redWord, redLoadPath, redMakeSeries,
+	redBinary, redString, redWord, redBlock, redLoadPath, redMakeSeries,
     redGet, redSet, redSetPath, redGetPath, redPrint, redProbe, redFormError,
     redOpenLogWindow, redCloseLogWindow, redOpenLogFile, redCloseLogFile
 
